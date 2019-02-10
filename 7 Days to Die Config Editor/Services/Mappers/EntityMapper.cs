9b3b3807fd8fd182001d2e58533e219b59270828
Nate-Reynolds <nlreynolds@gmail.com>
2019-02-09 23:14:10 -0500

    Stub in Services project

---
+++ 7 Days to Die Config Editor/Services/Mappers/EntityMapper.cs
@@ -0,0 +1,19 @@
+﻿using Domain;
+using Services.Mappers;
+using System;
+
+namespace Services
+{
+    public class EntityMapper : IMapper<entity_classesEntity_class, Entity>
+    {
+        public entity_classesEntity_class ConvertFrom(Entity domainSource)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Entity ConvertTo(entity_classesEntity_class xmlSource)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 7 Days to Die Config Editor/Services/Mappers/IMapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Mappers
+{
+    public interface IMapper<X, T> 
+        where X : class 
+        where T : class
+    {
+        X ConvertFrom(T domainSource);
+        T ConvertTo(X xmlSource);
+    }
+}
