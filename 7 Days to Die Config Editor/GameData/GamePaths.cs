98f6613d0effb7f8bbce14b1a56e85fb8edd63d7
Nate Reynolds <Nate.Reynolds@cropins.net>
2019-02-23 19:21:47 -0500

    Added GamePaths class for maintaining game paths

---
+++ 7 Days to Die Config Editor/GameData/GamePaths.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameData
+{
+    public class GamePaths
+    {
+        public string GameRoot => @"7 Days To Die";
+        public string DataRoot => $@"{GameRoot}\Data";
+        public string ConfigRoot => $@"{DataRoot}\Config";
+
+        public string EntityClassesFilename => "entityclasses.xml";
+
+        public string EntityClassesPath => $@"{ConfigRoot}\{EntityClassesFilename}";
+    }
+}
