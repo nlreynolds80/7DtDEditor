ba8b30173666b7fe6e0209eaa14f53209fbb90db
Nate Reynolds <Nate.Reynolds@cropins.net>
2019-02-24 21:33:40 -0500

    JsonSerializationService

---
+++ 7 Days to Die Config Editor/Services/Serializers/JsonSerializationService.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Serializers
+{
+    public class JsonSerializationService : ISerializationService
+    {
+        public T Deserialize<T>(string source) where T : class
+        {
+            return JsonConvert.DeserializeObject<T>(source);
+        }
+
+        public string Serialize<T>(T source) where T : class
+        {
+            return JsonConvert.SerializeObject(source);
+        }
+    }
+}
