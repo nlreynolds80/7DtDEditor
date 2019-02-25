194463f815169cf0696bb8c59f4b33b02fb24433
Nate Reynolds <Nate.Reynolds@cropins.net>
2019-02-24 22:48:54 -0500

    IsolatedStorageFileService

---
+++ 7 Days to Die Config Editor/Services/Storage/IsolateStorageFileService.cs
@@ -0,0 +1,32 @@
+﻿using Services.Extensions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Text;
+
+namespace Services.Storage
+{
+    public class IsolateStorageFileService : ILocalFileService
+    {
+        public string Get(string filePath)
+        {
+            var userIsolatedStorageFile = IsolatedStorageFile.GetUserStoreForAssembly();
+            using (var userIsolatedStorageStream = new IsolatedStorageFileStream(filePath, FileMode.OpenOrCreate, userIsolatedStorageFile))
+            {
+                return userIsolatedStorageStream.ReadToString();
+            }
+        }
+
+        public void Save(string data, string filePath)
+        {
+            var userIsolatedStorageFile = IsolatedStorageFile.GetUserStoreForAssembly();
+            using (var userIsolatedStorageStream = new IsolatedStorageFileStream(filePath, FileMode.Create, userIsolatedStorageFile))
+            using (var streamWriter = new StreamWriter(userIsolatedStorageStream))
+            {
+                streamWriter.Write(data);
+                streamWriter.Flush();
+            }
+        }
+    }
+}
