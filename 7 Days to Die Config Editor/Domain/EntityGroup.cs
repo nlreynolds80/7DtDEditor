ac40f6661fa12d84aab28e9f2e7d31e86be9963a
Nate Reynolds <Nate.Reynolds@cropins.net>
2019-03-09 23:01:47 -0500

    Add EntityGroup and EntityGroupSubscription classes

---
+++ 7 Days to Die Config Editor/Domain/EntityGroup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class EntityGroup
+    {
+        public string Name { get; }
+
+        public List<EntityGroupSubscription> EntitySubscriptions { get; private set; } = new List<EntityGroupSubscription>();
+
+        public EntityGroup(string name)
+        {
+            Name = name;
+        }
+
+        public void SetEntitySubscriptions(List<EntityGroupSubscription> entityGroupSubscriptions)
+        {
+            EntitySubscriptions = entityGroupSubscriptions ?? new List<EntityGroupSubscription>();
+        }
+
+        public void AddEntity(Entity entity, string probability)
+        {
+            if (EntitySubscriptions.Any(es => es.Entity == entity))
+                throw new ArgumentException($"Entity {entity.Name} already has a subscription");
+
+            EntitySubscriptions.Add(new EntityGroupSubscription(entity, probability));
+        }
+
+        public void RemoveEntity(Entity entity)
+        {
+            if (!EntitySubscriptions.Any(es => es.Entity == entity))
+                throw new ArgumentException($"Entity {entity.Name} not found in subscription");
+
+            var entityGroup = EntitySubscriptions.First(es => es.Entity == entity);
+            EntitySubscriptions.Remove(entityGroup);
+        }
+    }
+}
---
+++ 7 Days to Die Config Editor/Domain/EntityGroupSubscription.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain
+{
+    public class EntityGroupSubscription
+    {
+        public string Name => Entity.Name;
+
+        public Entity Entity { get; }
+
+        public string Probability { get; set; }
+
+        public EntityGroupSubscription(Entity entity, string probability)
+        {
+            Entity = entity;
+            Probability = probability;
+        }
+    }
+}
