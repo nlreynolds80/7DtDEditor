1af16f91cb7481e3adbebb095084d4a9fa29f6db
Nate Reynolds <Nate.Reynolds@cropins.net>
2019-03-10 22:06:55 -0400

    Added EntityGroupMapper and tests

---
+++ 7 Days to Die Config Editor/GameData/Mappers/EntityGroupMapper.cs
@@ -0,0 +1,33 @@
+﻿using Domain;
+using System;
+using System.Linq;
+
+namespace GameData.Mappers
+{
+    public class EntityGroupMapper : IMapper<entitygroupsEntitygroup, EntityGroup>
+    {
+        private readonly IMapper<entitygroupsEntitygroupEntity, EntityGroupSubscription> _entityGroupSubscriptionMapper;
+
+        public EntityGroupMapper(IMapper<entitygroupsEntitygroupEntity, EntityGroupSubscription> entityGroupSubscriptionMapper)
+        {
+            _entityGroupSubscriptionMapper = entityGroupSubscriptionMapper;
+        }
+
+        public entitygroupsEntitygroup Convert(EntityGroup domainSource)
+        {
+            var xml = new entitygroupsEntitygroup()
+            {
+                name = domainSource.Name,
+                entity = domainSource.EntitySubscriptions.Count > 0 ? domainSource.EntitySubscriptions.Select(e => _entityGroupSubscriptionMapper.Convert(e)).ToArray() : null
+            };
+            return xml;
+        }
+
+        public EntityGroup Convert(entitygroupsEntitygroup xmlSource)
+        {
+            var entityGroup = new EntityGroup(xmlSource.name);
+            entityGroup.SetEntitySubscriptions(xmlSource.entity.Select(e => _entityGroupSubscriptionMapper.Convert(e)).ToList());
+            return entityGroup;
+        }
+    }
+}
---
+++ 7 Days to Die Config Editor/Tests/Mappers/EntityGroupMapper_Tests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Domain;
+using GameData.Mappers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Tests.Mappers
+{
+    [TestClass]
+    public class EntityGroupMapper_Tests
+    {
+        private Mock<IMapper<entitygroupsEntitygroupEntity, EntityGroupSubscription>> _entityGroupSubscriptionMapper;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _entityGroupSubscriptionMapper = new Mock<IMapper<entitygroupsEntitygroupEntity, EntityGroupSubscription>>();
+            _entityGroupSubscriptionMapper.Setup(obj => obj.Convert(It.IsAny<EntityGroupSubscription>())).Returns(new entitygroupsEntitygroupEntity());
+            _entityGroupSubscriptionMapper.Setup(obj => obj.Convert(It.IsAny<entitygroupsEntitygroupEntity>()))
+                .Returns(new EntityGroupSubscription(new Entity("test"), "0"));
+        }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void ConvertToDomain_GivenValidObject_MapsCorrectly()
+        {
+            //Arrange
+            var entityGroupMapper = new EntityGroupMapper(_entityGroupSubscriptionMapper.Object);
+            var xml = new entitygroupsEntitygroup()
+            {
+                name = "test",
+                entity = new entitygroupsEntitygroupEntity[1]
+                {
+                    new entitygroupsEntitygroupEntity()
+                }
+            };
+
+            //Act
+            var entityGroup = entityGroupMapper.Convert(xml);
+
+            //Assert
+            Assert.IsInstanceOfType(entityGroup, typeof(EntityGroup));
+            Assert.AreEqual(1, entityGroup.EntitySubscriptions.Count);
+            Assert.AreEqual("test", entityGroup.Name);
+        }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void ConvertToXml_GivenValidObject_MapsCorrectly()
+        {
+            //Arrange
+            var entityGroupMapper = new EntityGroupMapper(_entityGroupSubscriptionMapper.Object);
+            var entityGroup = new EntityGroup("test");
+
+            //Act
+            var xml = entityGroupMapper.Convert(entityGroup);
+
+            //Assert
+            Assert.IsInstanceOfType(xml, typeof(entitygroupsEntitygroup));
+            Assert.AreEqual("test", xml.name);
+            Assert.IsNull(xml.entity);
+        }
+    }
+}
