941a0f5706674c9795d77c03f6e6a2cf9aa06e4f
Nate Reynolds <Nate.Reynolds@cropins.net>
2019-03-16 17:17:32 -0400

    Add Result class

---
+++ 7 Days to Die Config Editor/Services/Result.cs
@@ -0,0 +1,40 @@
+﻿namespace Services
+{
+    public class Result
+    {
+        public bool IsSuccess { get; }
+        public bool IsFailure => !IsSuccess;
+        public string Error { get; }
+
+        protected Result(bool isSuccess, string error)
+        {
+            IsSuccess = isSuccess;
+            Error = error;
+        }
+
+        public static Result Ok()
+        {
+            return new Result(true, null);
+        }
+
+        public static Result<T> Ok<T>(T result)
+        {
+            return new Result<T>(result);
+        }
+
+        public static Result Fail(string message)
+        {
+            return new Result(false, message);
+        }
+    }
+
+    public class Result<T> : Result
+    {
+        public T Value { get; }
+
+        protected internal Result(T value) : base(true, null)
+        {
+            Value = value;
+        }
+    } 
+}
---
+++ 7 Days to Die Config Editor/Tests/Result_Tests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Services;
+
+namespace Tests
+{
+    [TestClass]
+    public class Result_Tests
+    {
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void Ok_GivenObject_ReturnsCorrectResult()
+        {
+            //Arrange
+            var o = new Entity("test");
+
+            //Act
+            var result = Result.Ok(o);
+
+            //Arrange
+            Assert.AreEqual(o, result.Value);
+            Assert.IsTrue(result.IsSuccess);
+            Assert.IsFalse(result.IsFailure);
+            Assert.IsNull(result.Error);
+        }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void Ok_GivenNoReturn_ReturnsCorrectResult()
+        {
+            //Arrange
+            //Act
+            var result = Result.Ok();
+
+            //Arrange
+            Assert.IsTrue(result.IsSuccess);
+            Assert.IsFalse(result.IsFailure);
+            Assert.IsNull(result.Error);
+        }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void Fail_ReturnsCorrectResult()
+        {
+            //Arrange
+            //Act
+            var result = Result.Fail("error");
+
+            //Arrange
+            Assert.AreEqual("error", result.Error);
+            Assert.IsFalse(result.IsSuccess);
+            Assert.IsTrue(result.IsFailure);
+        }
+    }
+}
